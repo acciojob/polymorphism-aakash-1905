cb0de8380cf888a2ef6736d2bbd4631d72feb7a8
Aakash Kumar <aakashkumar051901@gmail.com>
2023-04-15 12:20:54 +0530

    overloading assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+static class Product{
+    public int product(int x, int y) {
+        return x+y;
+    };
+    public int product(int x, int y, int z) {
+        return x+y+z;
+    };
+    public double product(double x, double y) {
+        return 1.0 *(x+y);
+    };
+}
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(5,6));
+        System.out.println(p.product(5,6,7));
+        System.out.println(p.product(5.1,6.0));
+    }
 }
